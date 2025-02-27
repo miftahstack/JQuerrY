69f87c817c5bbca2db67e4d1cbc887948f0bf0c3
MIFTAH UL HAQ <miftah4165@gmail.com>
2025-02-27 14:55:10 +0600

    JS Changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,46 @@
-$(function () {
-$("#Sports").click (function(){
-    $("#banner_part").toggleClass("100")
-})    
-});
+$(function(){
+    $("#hide").click(function(){
+        $(".Box").hide(1000)
+    })
+})
+// ================
+
+$(function(){
+    $("#show").click(function(){
+        $(".Box").show(1000)
+    })
+})
+// ===============
+
+$(function(){
+    $("#Showhide").click(function(){
+        $(".Box").toggle(1000)
+    })
+})
+// ==============
+
+$(function(){
+    $("#FadeOut").click(function(){
+        $(".Box").fadeOut(1000)
+    })
+
+    $("#FadeIn").click(function(){
+        $(".Box").fadeIn(1000)
+    })
+    $("#FlidInOut").click(function(){
+        $(".Box").fadeToggle(1000)
+    })
+})
+// ===============
+$(function(){
+    $("#SlideDown").click(function(){
+        $(".Box").slideDown(1000)
+    })
+    $("#SlideUp").click(function(){
+        $(".Box").slideUp(1000)
+    })
+    $("#SlideUpDown").click(function(){
+        $(".Box").slideToggle(1000)
+    })
+})
+
